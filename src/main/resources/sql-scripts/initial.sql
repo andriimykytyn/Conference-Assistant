87b5020a3d8c5a6f83217a67e7c7668369321816
andrew <andriymykytyn@yahoo.com>
2018-12-03 22:32:05 +0200

    database naming adjustments

--- src/main/resources/sql-scripts/initial.sql
+++ src/main/resources/sql-scripts/initial.sql
@@ -1,49 +1,49 @@
 CREATE DATABASE IF NOT EXISTS conferences;
 USE conferences;
 
-CREATE TABLE IF NOT EXISTS users (
-  user_id INT NOT NULL,
-  user_name VARCHAR(45) NOT NULL,
-  email VARCHAR(45) NOT NULL,
-  role ENUM('USER', 'ADMIN', 'MODER') NOT NULL,
-  surname VARCHAR(45) NULL,
-  nickname VARCHAR(45) NULL,
-  PRIMARY KEY (user_id)
+CREATE TABLE IF NOT EXISTS users_usr (
+  id_usr INT NOT NULL,
+  firstname_usr VARCHAR(45) NOT NULL,
+  lastname_usr VARCHAR(45) NULL,
+  email_usr VARCHAR(45) NOT NULL,
+  role_usr ENUM('USER', 'ADMIN', 'MODER') NOT NULL,
+  nickname_usr VARCHAR(45) NULL,
+  PRIMARY KEY (id_usr)
 ) ENGINE = InnoDB;
 
 
-CREATE TABLE IF NOT EXISTS conferences (
-  conference_id INT NOT NULL,
-  conference_name VARCHAR(45) NOT NULL,
-  conference_info TEXT NOT NULL,
-  PRIMARY KEY (conference_id)
+CREATE TABLE IF NOT EXISTS conferences_cf (
+  id_cf INT NOT NULL,
+  name_cf VARCHAR(45) NOT NULL,
+  info_cf TEXT NOT NULL,
+  PRIMARY KEY (id_cf)
 ) ENGINE = InnoDB;
 
-CREATE TABLE IF NOT EXISTS reports (
-  report_id INT NOT NULL,
-  report_name VARCHAR(45) NOT NULL,
-  announcer_info TEXT NOT NULL,
-  fk_conference_id INT NOT NULL,
-  PRIMARY KEY (report_id),
-  FOREIGN KEY (fk_conference_id) REFERENCES conferences(conference_id)
+CREATE TABLE IF NOT EXISTS reports_rp (
+  id_rp INT NOT NULL,
+  name_rp VARCHAR(45) NOT NULL,
+  info_rp TEXT NOT NULL,
+  fk_id_cf INT NOT NULL,
+  PRIMARY KEY (id_rp),
+  FOREIGN KEY (fk_id_cf) REFERENCES conferences_cf(id_cf)
 ) ENGINE = InnoDB;
 
-CREATE TABLE IF NOT EXISTS questions (
-  question_id INT NOT NULL,
-  question TEXT NULL,
-  answer TEXT NULL,
-  rating INT NULL,
-  fk_report_id INT NOT NULL,
-  fk_user_id INT NOT NULL,
-  PRIMARY KEY(question_id),
-  FOREIGN KEY(fk_report_id) REFERENCES reports(report_id),
-  FOREIGN KEY(fk_user_id) REFERENCES users(user_id)
+CREATE TABLE IF NOT EXISTS questions_qs (
+  id_qs INT NOT NULL,
+  question_qs TEXT NULL,
+  answer_qs TEXT NULL,
+  rating_qs INT NULL,
+  fk_id_rp INT NOT NULL,
+  fk_id_usr INT NOT NULL,
+  PRIMARY KEY(id_qs),
+  FOREIGN KEY(fk_id_rp) REFERENCES reports_rp(id_rp),
+  FOREIGN KEY(fk_id_usr) REFERENCES users_usr(id_usr)
 ) ENGINE = InnoDB;
 
-CREATE TABLE IF NOT EXISTS users_has_conferences (
-  users_id_user INT NOT NULL,
-  conferences_id_conference INT NOT NULL,
-  PRIMARY KEY (users_id_user, conferences_id_conference),
-  FOREIGN KEY (users_id_user) REFERENCES users(user_id),
-  FOREIGN KEY (conferences_id_conference) REFERENCES conferences(conference_id)
+CREATE TABLE IF NOT EXISTS usr_has_cf (
+  usr_id_usr INT NOT NULL,
+  cf_id_cf INT NOT NULL,
+  PRIMARY KEY (usr_id_usr, cf_id_cf),
+  FOREIGN KEY (usr_id_usr) REFERENCES users_usr(id_usr),
+  FOREIGN KEY (cf_id_cf) REFERENCES conferences_cf(id_cf)
 ) ENGINE = InnoDB;
