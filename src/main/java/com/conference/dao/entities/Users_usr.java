1412c3f0d9ef9c60b54688aa856b0e43a8feb4a6
andrew <andriymykytyn@yahoo.com>
2018-12-11 15:55:08 +0200

    added SignupServlet \ html files adjustments

--- src/main/java/com/conference/dao/entities/Users_usr.java
+++ src/main/java/com/conference/dao/entities/Users_usr.java
@@ -2,8 +2,6 @@ package com.conference.dao.entities;
 
 public class Users_usr {
     private int id_usr;
-    private String firstname_usr;
-    private String lastname_usr;
     private String email_usr;
     //'USER','ADMIN','MODER'
     private String role_usr;
@@ -15,8 +13,6 @@ public class Users_usr {
 
     public Users_usr(int id_usr, String firstname_usr, String lastname_usr, String email_usr, String role_usr, String nickname_usr, String password_usr) {
         this.id_usr = id_usr;
-        this.firstname_usr = firstname_usr;
-        this.lastname_usr = lastname_usr;
         this.email_usr = email_usr;
         this.role_usr = role_usr;
         this.nickname_usr = nickname_usr;
@@ -29,22 +25,6 @@ public class Users_usr {
 
     public void setId_usr(int id_usr) {
         this.id_usr = id_usr;
-    }
-
-    public String getFirstname_usr() {
-        return firstname_usr;
-    }
-
-    public void setFirstname_usr(String firstname_usr) {
-        this.firstname_usr = firstname_usr;
-    }
-
-    public String getLastname_usr() {
-        return lastname_usr;
-    }
-
-    public void setLastname_usr(String lastname_usr) {
-        this.lastname_usr = lastname_usr;
     }
 
     public String getEmail_usr() {
@@ -83,8 +63,6 @@ public class Users_usr {
     public String toString() {
         return "User{" +
                 "id=" + id_usr +
-                ", firstname='" + firstname_usr + '\'' +
-                ", lastname='" + lastname_usr + '\'' +
                 ", email='" + email_usr + '\'' +
                 ", role='" + role_usr + '\'' +
                 ", nickname='" + nickname_usr + '\'' +
