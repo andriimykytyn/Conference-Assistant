070df45db811f8ec7c172abe5652732ced56fb40
roman <19roman23@gmail.com>
2018-12-12 11:46:46 +0200

    create table for database conference

--- src/main/resources/sql-scripts/initial.sql
+++ src/main/resources/sql-scripts/initial.sql
@@ -3,8 +3,7 @@ USE conferences;
 
 CREATE TABLE IF NOT EXISTS users_usr (
   id_usr INT NOT NULL,
-  firstname_usr VARCHAR(45) NOT NULL,
-  lastname_usr VARCHAR(45) NULL,
+  name_usr VARCHAR(45) NOT NULL,
   email_usr VARCHAR(45) NOT NULL,
   role_usr ENUM('USER', 'ADMIN', 'MODER') NOT NULL,
   nickname_usr VARCHAR(45) NULL,
@@ -23,9 +22,10 @@ CREATE TABLE IF NOT EXISTS reports_rp (
   id_rp INT NOT NULL,
   name_rp VARCHAR(45) NOT NULL,
   info_rp TEXT NOT NULL,
-  fk_id_cf INT NOT NULL,
-  PRIMARY KEY (id_rp),
-  FOREIGN KEY (fk_id_cf) REFERENCES conferences_cf(id_cf)
+  announcer_info TEXT NOT NULL,
+  fk_conference_id INT,
+  PRIMARY KEY (id_rp)
+  /*FOREIGN KEY (fk_id_cf) REFERENCES conferences_cf(id_cf)*/
 ) ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS questions_qs (
@@ -35,15 +35,7 @@ CREATE TABLE IF NOT EXISTS questions_qs (
   rating_qs INT NULL,
   fk_id_rp INT NOT NULL,
   fk_id_usr INT NOT NULL,
-  PRIMARY KEY(id_qs),
-  FOREIGN KEY(fk_id_rp) REFERENCES reports_rp(id_rp),
-  FOREIGN KEY(fk_id_usr) REFERENCES users_usr(id_usr)
-) ENGINE = InnoDB;
-
-CREATE TABLE IF NOT EXISTS usr_has_cf (
-  usr_id_usr INT NOT NULL,
-  cf_id_cf INT NOT NULL,
-  PRIMARY KEY (usr_id_usr, cf_id_cf),
-  FOREIGN KEY (usr_id_usr) REFERENCES users_usr(id_usr),
-  FOREIGN KEY (cf_id_cf) REFERENCES conferences_cf(id_cf)
+  PRIMARY KEY(id_qs)
+  /*FOREIGN KEY(fk_id_rp) REFERENCES reports_rp(id_rp),
+  FOREIGN KEY(fk_id_usr) REFERENCES users_usr(id_usr)*/
 ) ENGINE = InnoDB;
